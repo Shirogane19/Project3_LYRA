5e9a97641ff37eb10920cd906943c8cbb625a4d0
wendy villegas <wendy villegas>
2016-04-04 21:03:46 -0600

    Configuracion materia X Grado 100% lista

--- src/main/java/com/ironthrone/lyra/services/GradoService.java
+++ src/main/java/com/ironthrone/lyra/services/GradoService.java
@@ -12,15 +12,19 @@ import org.springframework.stereotype.Service;
 import com.ironthrone.lyra.contracts.GradoRequest;
 import com.ironthrone.lyra.ejb.Grado;
 import com.ironthrone.lyra.ejb.Institucion;
+import com.ironthrone.lyra.ejb.Materia;
 import com.ironthrone.lyra.pojo.GradoPOJO;
+import com.ironthrone.lyra.pojo.MateriaPOJO;
 import com.ironthrone.lyra.repositories.GradoRepository;
 import com.ironthrone.lyra.repositories.InstitucionRepository;
+import com.ironthrone.lyra.repositories.MateriaRepository;
 
 @Service
 public class GradoService implements GradoServiceInterface {
 	
-	@Autowired GradoRepository gradeRepository;
+	@Autowired private GradoRepository gradeRepository;
 	@Autowired private InstitucionRepository institucionRepository;
+	@Autowired private MateriaRepository materiaRepository;
 	
 	
 	/**
@@ -36,7 +40,7 @@ public class GradoService implements GradoServiceInterface {
 			GradoPOJO dto = new GradoPOJO();
 			BeanUtils.copyProperties(g,dto);
 			dto.setActiveGr(g.getIsActiveGr());
-			dto.setMaterias(null);
+			dto.setMaterias(generateMateriaDtos(g.getMaterias()));
 			dto.setSeccions(null);
 			uiGrados.add(dto);
 		});	
@@ -70,7 +74,7 @@ public class GradoService implements GradoServiceInterface {
 		
 		BeanUtils.copyProperties(grado,dto);
 		dto.setActiveGr(grado.getIsActiveGr());
-		dto.setMaterias(null);
+		dto.setMaterias(generateMateriaDtos(grado.getMaterias()));
 		dto.setSeccions(null);
 		
 		return dto;
@@ -98,26 +102,41 @@ public class GradoService implements GradoServiceInterface {
 		
 		Grado newGrado = new Grado();
 		Grado ngrade = null;
-		
-		/**
-		 * Seguir los mismos
-		 * pasos que en usuarioService
-		 * mas adelante para agregarle secciones
-		 * y las materias.
-		 */
+		List<String> idMaterias = gr.getGrado().getIdMaterias();
+		List<Materia> materias = new ArrayList<Materia>();
+		Boolean hasMaterias = false; 
 		BeanUtils.copyProperties(gr.getGrado(), newGrado);
 		
 		int idInstitucion = gr.getGrado().getInstitucion().getIdInstitucion();
 		Institucion i = institucionRepository.findOne(idInstitucion);
 		newGrado.setInstitucion(i);
 		
+		
+		if(idMaterias != null){
+				if(!idMaterias.isEmpty()){
+				hasMaterias = true;
+			}					
+		}
+
+		
 		if(gr.getGrado().getIdGrado() <= -1){
 			newGrado.setIdGrado(0);
 			newGrado.setIsActiveGr(true);
 			ngrade = gradeRepository.save(newGrado);
+			if(hasMaterias){
+				ngrade = assignMateriaGrado(idMaterias, ngrade , materias);
+			}
 			
 		}else{		
 			Grado oldGrado = findById(gr.getGrado().getIdGrado());
+			oldGrado = removeMaterias(oldGrado);
+			if(hasMaterias){
+				
+				materias = getMaterias(idMaterias);
+				oldGrado.setMaterias(materias);	
+			}
+			
+				
 			oldGrado = assignProperties(oldGrado, gr.getGrado());
 			ngrade = gradeRepository.save(oldGrado);
 			
@@ -127,6 +146,28 @@ public class GradoService implements GradoServiceInterface {
 	}
 
 	
+	private Grado assignMateriaGrado(List<String> idMaterias, Grado ngrade, List<Materia> materias) {
+		materias = getMaterias(idMaterias);
+		ngrade.setMaterias(materias);
+		ngrade  = gradeRepository.save(ngrade);
+		return ngrade;
+	}
+
+
+	private List<Materia> getMaterias(List<String> idMaterias) {
+		List<Materia> materias = new ArrayList<Materia>();
+		
+		idMaterias.stream().forEach(m -> {	
+			
+			int id = Integer.parseInt(m);
+			Materia mate = materiaRepository.findOne(id);
+			materias.add(mate);
+		
+		});
+		return materias;
+	}
+
+
 	/**
 	 * Copia los nuevos datos traidos de la IU a el objeto que se va a salvar en la base
 	 * @param DbGrado grado que se salvara a la base
@@ -141,6 +182,38 @@ public class GradoService implements GradoServiceInterface {
 		DbGrado.setIsActiveGr(UiGrado.getIsActiveGr());
 		
 		return DbGrado;
+	}
+	
+	/**
+	 * 
+	 * @param oldGra
+	 * @return
+	 */
+	private Grado removeMaterias(Grado oldGra) {
+		oldGra.setMaterias(null);
+		oldGra = gradeRepository.save(oldGra);
+		
+		return oldGra;
+	}
+	
+	/**
+	 * Genera POJOs a partir de una lista EJB.
+	 * @param materias representa una lista de materia tipo ejb
+	 * @return UserInterfaceMaterias representa una lista de materia tipo POJO.
+	 */
+	private List<MateriaPOJO> generateMateriaDtos(List<Materia> materias) {
+		
+		List<MateriaPOJO> uiMateria = new ArrayList<MateriaPOJO>();
+
+		materias.stream().forEach(m -> {
+			MateriaPOJO dto = new MateriaPOJO();
+			dto.setIdMateria(m.getIdMateria());
+			dto.setNombre(m.getNombre());
+			dto.setActiveMat(m.getIsActiveMat());
+			uiMateria.add(dto);
+		});	
+		
+		return uiMateria;
 	}
 	
 	
